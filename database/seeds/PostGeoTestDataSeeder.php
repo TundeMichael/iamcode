03e35922763aaf519de70f6f6c57d4f8780dae78
Enochval <enoch@bincom.net>
2017-11-19 14:53:34 +0100

    [route] feed route

--- database/seeds/PostGeoTestDataSeeder.php
+++ database/seeds/PostGeoTestDataSeeder.php
@@ -32,8 +32,33 @@ class PostGeoTestDataSeeder extends Seeder {
             'user_id' => 9000, 'media_url' => 'google.com', 'gps' => 0,
             'description' => 'test description 2',
             'location' => '45.3681,7.7681'));
-        
-        
+
+        Post::create(array('category_id' => 9001,
+            'user_id' => 9000, 'media_url' => 'google.com', 'gps' => 0,
+            'description' => 'test description 3',
+            'location' => '45.3681,7.7681'));
+        Post::create(array('category_id' => 9001,
+            'user_id' => 9000, 'media_url' => 'google.com', 'gps' => 0,
+            'description' => 'test description 4',
+            'location' => '45.3681,7.7681'));
+
+        Post::create(array('category_id' => 9002,
+            'user_id' => 9000, 'media_url' => 'google.com', 'gps' => 0,
+            'description' => 'test description 5',
+            'location' => '45.3681,7.7681'));
+        Post::create(array('category_id' => 9002,
+            'user_id' => 9000, 'media_url' => 'google.com', 'gps' => 0,
+            'description' => 'test description 6',
+            'location' => '45.3681,7.7681'));
+
+        Post::create(array('category_id' => 9003,
+            'user_id' => 9000, 'media_url' => 'google.com', 'gps' => 0,
+            'description' => 'test description 7',
+            'location' => '45.3681,7.7681'));
+        Post::create(array('category_id' => 9003,
+            'user_id' => 9000, 'media_url' => 'google.com', 'gps' => 0,
+            'description' => 'test description 8',
+            'location' => '45.3681,7.7681'));
         
     }
 
