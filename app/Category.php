de32fe34374b3c7b658d311d92ff7799af6be25c
Enochval <enoch@bincom.net>
2017-11-19 14:44:36 +0100

    [route] feed route

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Category extends Model
 {
-    //
+    public function categories()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Category;
 
 class Post extends PostModel
 {
-    //
+    public function posts()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
